ca78137b16968a2fb51d4af183c29fd334efa6e6
Marcin Rajchel <rajchel.marcin@gmail.com>
2025-11-09 22:07:29 +0100

    order component

---
+++ src/app/_components/order/order.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'flexmile-order',
+  imports: [],
+  templateUrl: './order.html',
+  styleUrl: './order.scss',
+})
+export class Order {
+
+}
