f959e9d689b6d8b9303919ddf01c63cce17b5396
Marcin Rajchel <rajchel.marcin@gmail.com>
2025-11-18 21:50:24 +0100

    optimalization

--- src/app/_components/home-page/_components/homepage-list/homepage-list.ts
+++ src/app/_components/home-page/_components/homepage-list/homepage-list.ts
@@ -1,11 +1,18 @@
-import { Component } from '@angular/core';
+import {Component, input} from '@angular/core';
+import {OfferListModel, OfferModel} from '@models/offers.types';
+import {badgeSizes} from '@models/common.types';
+import {Car} from '@components/utilities/car/car';
 
 @Component({
   selector: 'flexmile-homepage-list',
-  imports: [],
+  imports: [
+    Car
+  ],
   templateUrl: './homepage-list.html',
   styleUrl: './homepage-list.scss',
 })
 export class HomepageList {
+  offerList = input<OfferListModel>();
+  protected readonly badgeSizes = badgeSizes;
 
 }
