60651a3f9e9cafc6c77de04969e78547498f61f5
Marcin Rajchel <rajchel.marcin@gmail.com>
2026-01-20 16:55:24 +0100

    tsconfig

--- src/environments/environment.dev.ts
+++ src/environments/environment.dev.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'https://api.flexmile.mr-creations.pl/wp-json/flexmile/v1',
   buildType: 'dev',
-  buildtime: 'Thu Jan 15 2026 15:16:58 GMT+0100 (Central European Standard Time)'
+  buildtime: 'Mon Jan 19 2026 17:54:20 GMT+0100 (Central European Standard Time)'
 };
 
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -2,6 +2,6 @@ export const environment = {
   production: false,
   apiUrl: 'http://flexmile.local/wp-json/flexmile/v1',
   buildType: 'local',
-  buildtime: 'Thu Jan 15 2026 15:16:58 GMT+0100 (Central European Standard Time)'
+  buildtime: 'Mon Jan 19 2026 17:54:20 GMT+0100 (Central European Standard Time)'
 };
 
